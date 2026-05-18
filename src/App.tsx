14b3d09d299c00bf409606742f34fe933d91e715
abdallamohamed55v-cmyk <abdallamohamed55v@gmail.com>
2026-05-18 22:11:12 +0300

    Update src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import Biography from "./pages/Biography";
+import Songs from "./pages/Songs";
+import Timeline from "./pages/Timeline";
+import Collaborations from "./pages/Collaborations";
+import Gallery from "./pages/Gallery";
+import Quotes from "./pages/Quotes";
+import Sources from "./pages/Sources";
 
 const queryClient = new QueryClient();
 
@@ -14,7 +21,14 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="/biography" element={<Biography />} />
+          <Route path="/songs" element={<Songs />} />
+          <Route path="/timeline" element={<Timeline />} />
+          <Route path="/collaborations" element={<Collaborations />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/quotes" element={<Quotes />} />
+          <Route path="/sources" element={<Sources />} />
+          {/* Catch-all */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
